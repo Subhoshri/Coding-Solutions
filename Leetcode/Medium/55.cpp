18fa5ba8b557682578ce0773ce2a40a7e02874dd
Subhoshri Pal <138369802+Subhoshri@users.noreply.github.com>
2025-03-02 14:55:27 +0530

    Create 55.cpp

---
+++ Leetcode/Medium/55.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal=nums.size()-1;
+        int i=nums.size()-2;
+        while(i>=0){
+            if(i+nums[i]>=goal){
+                goal=i;
+                i--;
+            }
+            else i--;
+        }
+        if (goal==0) return true;
+        return false;
+    }
+};
